133c50cb3631c7241d12ff2a56e9743cb0a710cc
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-27 05:46:01 -0600

    pint test

--- pint.c
+++ pint.c
@@ -8,9 +8,9 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top;
 
-	top = *stack + 1;
+	top = *stack;
 
-	if (top == NULL)
+	if (*top == NULL)
 	{
 		printf("L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
